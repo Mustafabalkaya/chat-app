84fa466a2e7372919fb7dd837809e8181630652b
Mustafabalkaya <106041095+Mustafabalkaya@users.noreply.github.com>
2024-04-08 20:48:26 +0300

    Update SocketContext.jsx

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chat-app-mern-4ns8.onrender.com", {
+			const socket = io("https://chat-app-xpgp.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
